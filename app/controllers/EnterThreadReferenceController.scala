aec502e0c5f171d3577c2517e4feb9594c0ede1c
ramindur.singh <282824933+ramindursingh@users.noreply.github.com>
2026-08-11 15:34:58 +0100

    SDEC-391: Getting external user information

--- app/controllers/EnterThreadReferenceController.scala
+++ app/controllers/EnterThreadReferenceController.scala
@@ -20,29 +20,36 @@ import controllers.actions.IdentifierAction
 import forms.models.ThreadReferenceForm
 import forms.providers.ThreadReferenceFormProvider
 import models.Mode
+import models.sdec.ExternalUser
 import play.api.Logging
 import play.api.data.Form
 import play.api.http.Status as HttpStatus
 import play.api.i18n.{I18nSupport, Messages}
 import play.api.mvc.*
 import service.ThreadReferenceServiceAlgebra
+import uk.gov.hmrc.auth.core.retrieve.v2.Retrievals
+import uk.gov.hmrc.auth.core.{AuthConnector, AuthorisedFunctions, Enrolments}
 import uk.gov.hmrc.http.{NotFoundException, UpstreamErrorResponse}
 import uk.gov.hmrc.play.bootstrap.frontend.controller.FrontendBaseController
-import views.html.{EnterThreadReferenceView, ThreadReferenceView}
+import views.html.{EnterThreadReferenceView, ThreadReferenceView, UnauthorisedView}
+import uk.gov.hmrc.auth.core.retrieve.~
 
 import javax.inject.Inject
 import scala.concurrent.{ExecutionContext, Future}
 
 class EnterThreadReferenceController @Inject() (
     val controllerComponents: MessagesControllerComponents,
-    identify: IdentifierAction,
+    val authConnector: AuthConnector,
+    identifierAction: IdentifierAction,
     enterThreadReferenceView: EnterThreadReferenceView,
     formProvider: ThreadReferenceFormProvider,
     threadReferenceView: ThreadReferenceView,
-    threadReferenceService: ThreadReferenceServiceAlgebra
+    threadReferenceService: ThreadReferenceServiceAlgebra,
+    unauthorisedView: UnauthorisedView
 )(using ec: ExecutionContext)
     extends FrontendBaseController
     with I18nSupport
+    with AuthorisedFunctions
     with Logging {
 
   private val form: Form[ThreadReferenceForm] = formProvider()
@@ -50,23 +57,83 @@ class EnterThreadReferenceController @Inject() (
   def onPageLoad(
       mode: Mode,
       threadReferenceForm: Form[ThreadReferenceForm] = form
-  ): Action[AnyContent] = identify { implicit request =>
-    Ok(enterThreadReferenceView(threadReferenceForm, mode))
-  }
+  ): Action[AnyContent] = {
+    Action.async { implicit request =>
+      implicit val messages: Messages = messagesApi.preferred(request)
 
-  def onContinue(mode: Mode): Action[AnyContent] =
-    identify.async { implicit request =>
-      val formData = form.bindFromRequest()
-      formData.value
-        .filter(t => formProvider.validateThreadReference(t.reference))
-        .fold(
-          Future.successful(returnBadRequest(formData, mode))
-        ) { tr =>
-          getThreadInformation(formData, mode, tr)
+      authorised()
+        .retrieve(
+          Retrievals.externalId and
+            Retrievals.nino and
+            Retrievals.allUserDetails and
+            Retrievals.authorisedEnrolments
+        ) { case externalId ~ nino ~ userDetails ~ enrolments =>
+          val user = ExternalUser(
+            externalId = externalId,
+            nino = nino,
+            userDetails = userDetails,
+            enrolments = enrolments
+          )
+          Future.successful(
+            Ok(
+              enterThreadReferenceView(
+                user: ExternalUser,
+                threadReferenceForm,
+                mode
+              )
+            )
+          )
+        }
+        .recoverWith { e =>
+          logger.warn(s"Unauthorised access: ${e.getMessage}")
+          Future.successful(Ok(unauthorisedView()))
         }
     }
+  }
+
+  def onContinue(mode: Mode): Action[AnyContent] = {
+    Action.async { implicit request =>
+      authorised()
+        .retrieve(
+          Retrievals.externalId and Retrievals.nino and Retrievals.allUserDetails and Retrievals.authorisedEnrolments
+        ) {
+          case externalId ~ nino ~ userDetails ~ enrolments => {
+            val formData = form.bindFromRequest()
+            val user     = ExternalUser(
+              externalId = externalId,
+              nino = nino,
+              userDetails = userDetails,
+              enrolments = enrolments
+            )
+            formData.value
+              .filter(t => formProvider.validateThreadReference(t.reference))
+              .fold(
+                Future.successful(
+                  returnBadRequest(
+                    user: ExternalUser,
+                    formData,
+                    mode
+                  )
+                )
+              )(tr =>
+                getThreadInformation(
+                  user: ExternalUser,
+                  formData,
+                  mode,
+                  tr
+                )
+              )
+          }
+        }
+        .recoverWith { e =>
+          logger.warn(s"Unauthorised access: ${e.getMessage}")
+          Future.successful(Ok(unauthorisedView()))
+        }
+    }
+  }
 
   private def getThreadInformation(
+      user: ExternalUser,
       form: Form[ThreadReferenceForm],
       mode: Mode,
       trForm: ThreadReferenceForm
@@ -81,26 +148,54 @@ class EnterThreadReferenceController @Inject() (
           val formWithError =
             form.withGlobalError(Messages("sdec.enterthreadref.api.notfound"))
           logger.warn(s"Thread Reference Not found: ${trForm.reference}")
-          NotFound(enterThreadReferenceView(formWithError, mode))
+          NotFound(
+            enterThreadReferenceView(
+              user: ExternalUser,
+              formWithError,
+              mode
+            )
+          )
         case e: UpstreamErrorResponse if e.statusCode == HttpStatus.NOT_FOUND =>
           logger.warn(s"Thread Reference Not found: ${trForm.reference}")
           val formWithError =
             form.withGlobalError(Messages("sdec.enterthreadref.api.notfound"))
-          NotFound(enterThreadReferenceView(formWithError, mode))
+          NotFound(
+            enterThreadReferenceView(
+              user: ExternalUser,
+              formWithError,
+              mode
+            )
+          )
         case ex =>
           val formWithError =
             form.withGlobalError(Messages("sdec.enterthreadref.api.error"))
           logger.error("Failed to retrieve thread information", ex)
-          ServiceUnavailable(enterThreadReferenceView(formWithError, mode))
+          ServiceUnavailable(
+            enterThreadReferenceView(
+              user: ExternalUser,
+              formWithError,
+              mode
+            )
+          )
       }
 
-  private def returnBadRequest(form: Form[ThreadReferenceForm], mode: Mode)(using
+  private def returnBadRequest(
+      user: ExternalUser,
+      form: Form[ThreadReferenceForm],
+      mode: Mode
+  )(using
       request: Request[?]
   ): Result = {
     logger.warn(s"Returning bad request for ${form.value}")
     val formWithError =
       form.withGlobalError(Messages("sdec.enterthreadref.error.problem.message"))
-    BadRequest(enterThreadReferenceView(formWithError, mode))
+    BadRequest(
+      enterThreadReferenceView(
+        user: ExternalUser,
+        formWithError,
+        mode
+      )
+    )
   }
 
 }
